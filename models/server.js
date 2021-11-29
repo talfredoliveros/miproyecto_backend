33f3b615201904f14cfeb824b285984481ce97fe
talfredoliveros <73256594+talfredoliveros@users.noreply.github.com>
2021-11-28 22:35:20 -0500

    se actualiza proyecto de Backend

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
     
     constructor(){
         this.app = express();
-        this.port = process.env.PORTSERVER | 3000;
+        this.port = process.env.PORT | 3000;
         this.productsPath = '/api/products';
         this.emprendedorPath = '/api/emprendedor';
 
